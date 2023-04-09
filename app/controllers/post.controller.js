3a32431a6a04c0dc1e75d38fff28daefa6cabe25
andre-fajar-n <andrenugroho395@gmail.com>
2023-04-09 15:26:02 +0700

    add endpoint to update post

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -1,7 +1,6 @@
 import { validationResult } from "express-validator";
 import db from "../models/index.js";
 import { errorFormatter, errorResponse, successResponse } from "../utils/response.util.js"
-import { validHashtag } from "../utils/string.util.js";
 
 const User = db.user;
 const Post = db.post;
@@ -57,6 +56,75 @@ const create = async (req, res) => {
     })
 }
 
+const update = async (req, res) => {
+    const errValidation = validationResult(req).formatWith(errorFormatter);
+    if (!errValidation.isEmpty()) {
+        return res.status(422).json(errorResponse({message:errValidation.array()[0]}))
+    }
+
+    try {
+        // find user by id
+        var user = await User.findOne({
+            where: {
+                id: req.user.userId,
+            }
+        })
+        if (!user) {
+            return res.status(404).send(errorResponse({message: "User not found"}))
+        }
+
+        // find post by id
+        var currentPost = await Post.findOne({
+            where: {
+                id: req.params.postId,
+            }
+        })
+        if (!currentPost) {
+            return res.status(404).send(errorResponse({message: "Post not found"}))
+        }
+        if (currentPost.userId !== user.id) {
+            return res.status(403).send(errorResponse({message: "Post is not yours"}))
+        }
+
+        const postReq = {
+            image: req.body.image,
+            caption: req.body.caption,
+            tags: req.body.tags,
+        }
+
+        const updatePost = await Post.update(postReq,
+            {
+                where:{ id:req.params.postId },
+            }
+        )
+        if (!updatePost) throw ("Error while updating")
+
+        const returnUpdatePost = await Post.findOne({ where: { id: req.params.postId }})
+        if (!returnUpdatePost) throw ("Error while fetch updated data")
+
+        return res.send(successResponse({
+            message: "Successfully Update Post",
+            data: {
+                image: returnUpdatePost.image,
+                caption: returnUpdatePost.caption,
+                tags: returnUpdatePost.tags,
+                likes: returnUpdatePost.likes,
+                createdAt: returnUpdatePost.createdAt,
+                updatedAt: returnUpdatePost.updatedAt,
+                user: {
+                    name: user.name,
+                    username: user.username,
+                    email: user.email,
+                    photo: user.photo,
+                }
+            }
+        }));
+    } catch (error) {
+        return res.status(500).send(errorResponse({message: error.message || "Internal server error"}))
+    }
+}
+
 export default {
     create,
+    update,
 };
--- app/routes/post.route.js
+++ app/routes/post.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateToken } from "../utils/jwt.util.js";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import postController from "../controllers/post.controller.js";
 import { validHashtag } from "../utils/string.util.js";
 
@@ -17,6 +17,20 @@ postRouter.post("/", authenticateToken,
         return true
     }),
     postController.create,
+);
+
+postRouter.put("/:postId", authenticateToken,
+    param("postId").isInt().exists().withMessage("cannot be empty and must be positive integer"),
+    body("image").exists().isLength({min:1}).trim().withMessage("cannot be empty").isURL().trim().withMessage("must be an URL"),
+    body("caption").exists().isLength({min:1}).trim().withMessage("cannot be empty"),
+    body("tags").exists().isLength({min:1}).trim().withMessage("cannot be empty").custom(value => {
+        if (!validHashtag(value)) {
+            throw new Error("invalid format")
+        }
+
+        return true
+    }),
+    postController.update,
 );
 
 export default postRouter;
